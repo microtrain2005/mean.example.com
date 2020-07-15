57889901a88c6e26b79170b51f50b1e2b13c99c5
chad svastisalee <swasgrafx@gmail.com>
2020-07-15 10:34:15 -0500

    Update for CORS and ionicUsers

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(function(req, res, next) {
 //Session-based access control
 app.use(function(req,res,next){
   //Uncomment the following line to allow access to everything.
-  // return next();
+  return next();
 
   //Allow any endpoint that is an exact match. The server does not
   //have access to the hash so /auth and /auth#xxx would bot be considered 
@@ -115,7 +115,8 @@ app.use(function(req,res,next){
   var subs = [
     '/public/',
     '/api/auth/',
-    '/articles'
+    '/articles',
+    '/ionicUsers'
   ];
 
   //The query string provides a partial URL match beginning
